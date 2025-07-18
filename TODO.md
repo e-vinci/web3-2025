d469d6639664b8084d79d9013b9ac9bc90a1ede8
unknown <martin@joyouscoding.com>
2025-07-18 11:00:38 +0200

    basic structure and format

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+## Website
+
+- Deploy
+- Make a dummy first "fiche" for lesson 1
+- Give PVE rights
+
+## Content
+
+### First two lessons (back to web dev)
+
+- Build a mustache basic clone
+- Material to follow
+- Theory review
+- PR test
+
+## Infra
+
+Where to deploy for the students (if possible) - back + front
--- src/navigation.ts
+++ src/navigation.ts
@@ -3,116 +3,54 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
       text: 'Pages',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'Moodle',
+          href: 'https://moodle.vinci.be/course/view.php?id=494',
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Evaluation',
+          href: getPermalink('/homes/evaluation'),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'Enseignants',
+          href: getPermalink('/homes/teachers'),
         },
         {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: 'Objectifs du cours',
+          href: getPermalink('/homes/goals'),
         },
       ],
     },
     {
-      text: 'Landing',
+      text: 'Lessons',
       links: [
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
+          text: 'Liste des exercices',
           href: getBlogPermalink(),
         },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+          text: 'Web 2 reloade',
+          href: getPermalink('web-2-reloaded', 'post'),
         },
         {
-          text: 'Article (with MDX)',
+          text: 'On veut un chat !',
+          href: getPermalink('on-veut-un-chat', 'post'),
+        },
+        {
+          text: 'Vous êtes ici',
           href: getPermalink('markdown-elements-demo-post', 'post'),
         },
         {
-          text: 'Category Page',
+          text: 'Show me the money',
           href: getPermalink('tutorials', 'category'),
         },
         {
-          text: 'Tag Page',
+          text: 'Debug everywhere',
           href: getPermalink('astro', 'tag'),
         },
       ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
